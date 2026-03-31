66b02815b8c513767454576e149fedf6370bd18f
bishal6900 <bishalbudhathoki006@gmail.com>
2026-03-31 17:05:30 +0545

    Create AnimatedButton component

---
+++ src/components/AnimatedButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const AnimatedButton = () => {
+    return (
+        <button className="animated-button">Click Me</button>
+    );
+};
+
+export default AnimatedButton;
